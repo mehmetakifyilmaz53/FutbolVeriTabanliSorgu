61a3948c7a86da45091c2a03f217c4f8138c346b
mehmetakifyilmaz53 <mehmetakifyilmaz17054@gmail.com>
2023-06-07 19:35:27 +0300

    First Commit

---
+++ Deneme1/Deneme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme1
+{
+    public class Deneme
+    {
+        public int? TakımID { get; set;}
+        public string? TakımAdı { get; set; }
+        public string? TakRenk { get; set; }
+        public int? Taksampi { get; set; }
+    }
+}
---
+++ Deneme1/Program.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Deneme1
+{
+    public interface futbol
+    {
+        List<Deneme> GetAllData();
+        Deneme GetFutbolID(int id);
+        void Create(Deneme futbol);
+        void Update(Deneme futbol);
+        void Delete (int TakımID);
+    }
+
+    public class Takımlar1 : futbol
+    {
+
+        static SqlConnection GetSqlConnection()
+        {
+            string connectionString = @"Data Source=DESKTOP-41H4HUT\SQLEXPRESS; Initial Catalog=federasyon; Integrated Security=SSPI;";
+            return new SqlConnection(connectionString);
+        }
+       
+
+        public void Create(Deneme futbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int TakımID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Deneme> GetAllData()
+        {
+            List<Deneme> Futbolcular = null;
+            using (var connection = GetSqlConnection()) 
+            { 
+                try
+                {
+                    connection.Open();
+                    string sgl = "select * from Takımlar";
+                    SqlCommand command = new SqlCommand(sgl, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+                    Futbolcular = new List<Deneme>();
+
+                    while (reader.Read())
+                    {
+                        Futbolcular.Add(new Deneme()
+                        {
+                            TakımID = int.Parse(reader["TakımID"].ToString()),
+                            TakımAdı = reader["TakımAdı"].ToString(),
+                            TakRenk = reader["Renk1"].ToString(),
+                            Taksampi = int.Parse(reader["SampiyonlukSayısı"].ToString()),
+                          }
+
+                          );
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+            return Futbolcular;
+        }
+
+        public Deneme GetFutbolID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Deneme futbol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Takımlar2 : futbol
+    {
+        futbol _futbolcu;
+
+        public Takımlar2(futbol futbolcu1)
+        {
+            _futbolcu = futbolcu1;
+        }
+
+        public void Create(Deneme futbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int TakımID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Deneme> GetAllData()
+        {
+            return _futbolcu.GetAllData();
+        }
+
+        public Deneme GetFutbolID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Deneme futbol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var futbolcu1 = new Takımlar2(new Takımlar1());
+
+            var data = futbolcu1.GetAllData();
+
+            foreach (var item in data) 
+            {
+                Console.WriteLine($"Takım no: {item.TakımID} Takım ad: {item.TakımAdı} TakımRenk: {item.TakRenk} Şampiyonluk: {item.Taksampi}");
+            }
+        }
+    }
+}
